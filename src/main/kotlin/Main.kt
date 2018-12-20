73ab16f724ece9442054782ecdf8e1c786475bd2
yo-kondo <buronson92@gmail.com>
2018-12-20 23:19:12 +0900

    いい感じに修正

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,18 @@
 import java.io.File
 
 /**
- * 数字4桁のディレクトリ名から先頭の数字1桁を削除し、数字3桁のディレクトリ名に変更する。
+ * 数字4桁のディレクトリ名から先頭の数字1桁を削除し、数字3桁のディレクトリ名に変更します。
  */
 fun main(args: Array<String>) {
-    // 正規表現
-    val regex = Regex("""[0-9]{4}""")
-    // TODO:対象のディレクトリ名を設定
-    val path = """C:\hoge"""
 
-    val file = File(path)
-    // 正規表現に合致したディレクトリ名のみ抽出
-    val target = file.listFiles().filter { regex.matches(it.name) }
-    for (t in target) {
-        // 変更するディレクトリ（Fileクラスで作成）
-        val dest = File(path, t.name.substring(1, t.name.length))
-        // 名前の変更
-        t.renameTo(dest)
-    }
+    // 対象のディレクトリ名を設定
+    val path = """D:\work"""
+
+    File(path).listFiles()
+        // 正規表現に合致したディレクトリ名のみ抽出
+        .filter { Regex("""[0-9]{4}""").matches(it.name) }
+        .forEach {
+            // 変更するディレクトリ（Fileクラスで作成）を指定し、名前を変更
+            it.renameTo(File(path, it.name.substring(1, it.name.length)))
+        }
 }
