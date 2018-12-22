7c7c3c9604b9d137a6c38dfe19712541938255c4
yo-kondo <buronson92@gmail.com>
2018-12-22 23:13:36 +0900

    画像リンクのパス修正　追加

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,22 @@
 import java.io.File
 
+
 /**
- * 数字4桁のディレクトリ名から先頭の数字1桁を削除し、数字3桁のディレクトリ名に変更します。
+ * エントリポイント
  */
 fun main(args: Array<String>) {
 
     // 対象のディレクトリ名を設定
-    val path = """D:\work"""
+    val path = """D:\work\md"""
+
+    // TODO:changeDirectory(path)
+    changeImageLink(path)
+}
+
+/**
+ * 数字4桁のディレクトリ名から先頭の数字1桁を削除し、数字3桁のディレクトリ名に変更します。
+ */
+private fun changeDirectory(path: String) {
 
     File(path).listFiles()
         // 正規表現に合致したディレクトリ名のみ抽出
@@ -14,5 +24,20 @@ fun main(args: Array<String>) {
         .forEach {
             // 変更するディレクトリ（Fileクラスで作成）を指定し、名前を変更
             it.renameTo(File(path, it.name.substring(1, it.name.length)))
+        }
+}
+
+/**
+ * Markdownファイル内の画像リンクのパスを修正します。
+ */
+private fun changeImageLink(path: String) {
+
+    // 指定したパス内の、全てのディレクトリ、ファイルを取得
+    File(path).walkTopDown()
+        // 拡張子がmdのファイルを抽出
+        .filter { it.isFile && it.extension == "md" }
+        .forEach {
+            it.forEachLine {
+            }
         }
 }
